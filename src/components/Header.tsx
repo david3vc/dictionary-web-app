b4d029c377519cc32b51bb10d01fac5fcc4f0db4
david3vc <dveracabanillas@hotmail.com>
2023-03-12 22:52:48 -0500

    avance de header

---
+++ src/components/Header.tsx
@@ -0,0 +1,107 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGripLinesVertical } from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react";
+import styled from "styled-components";
+import iconoBook from '../assets/images/logo.svg'
+import iconoMoon from '../assets/images/icon-moon.svg'
+import iconArrowDown from '../assets/images/icon-arrow-down.svg'
+import Checkbox from './Checkbox'
+import '../styles/header.css'
+
+const Main = styled("div")`
+  font-family: sans-serif;
+  background: #f0f0f0;
+  height: 100vh;
+`;
+
+const DropDownContainer = styled("div")`
+  width: 5.5em;
+  margin: 0 auto;
+`;
+
+const DropDownHeader = styled("div")`
+  cursor: pointer;
+  background: #ffffff;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DropDownListContainer = styled("div")``;
+
+const DropDownList = styled("ul")`
+  position: relative;
+  right: 50px;
+  cursor: pointer;
+  width: 8.5em;
+  padding: 1em;
+  margin-top: 10px;
+  background: #ffffff;
+  box-sizing: border-box;
+  &:first-child {
+      padding-top: 0.8em;
+    }
+  border-radius: 15px;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+`;
+
+const ListItem = styled("li")`
+  list-style: none;
+  margin-bottom: 3px;
+  &:hover{
+    color: #A445ED;
+  }
+`;
+
+const options = ["San Serif", "Serif", "Mono"];
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const toggling = () => setIsOpen(!isOpen);
+
+  const onOptionClicked = (value: string) => () => {
+    setSelectedOption(value);
+    setIsOpen(false);
+    console.log(selectedOption);
+  };
+  return (
+    <div className='container-header'>
+        <div className="container-header__logo">
+            <img src={iconoBook} alt="" />
+        </div>
+        <div className="container-header__options">
+            <div className="options__typography">
+                <DropDownContainer>
+                    <DropDownHeader onClick={toggling} >
+                        {selectedOption || "San Serif"}
+                        <img src={iconArrowDown} alt="" />
+                    </DropDownHeader>
+                        {isOpen && (
+                            <DropDownListContainer>
+                                <DropDownList>
+                                {options.map(option => (
+                                    <ListItem onClick={onOptionClicked(option)} key={Math.random()}>
+                                    {option}
+                                    </ListItem>
+                                ))}
+                                </DropDownList>
+                            </DropDownListContainer>
+                        )}
+                </DropDownContainer>
+            </div>
+            <div className="options__icon-pipe">
+              <FontAwesomeIcon icon={faGripLinesVertical} />
+            </div>
+            <div className="options__swtich">
+                <Checkbox />
+            </div>
+            <div className="options__icono-moon">
+                <img src={iconoMoon} alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, far);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
